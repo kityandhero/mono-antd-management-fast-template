12987d0c37619604a5ee150b055e4b3aa2fa0796
kityandhero <kityandhero@126.com>
2024-08-01 11:43:19 +0800

    fix(management): :bug: update

--- packages/management/config/router.config.js
+++ packages/management/config/router.config.js
@@ -1,9 +1,9 @@
-import { dashboard, root } from './router.master.custom.config';
+import { dashboard, root } from './router.custom.config';
 import {
   currentAccount,
   entrance,
   notFound,
   result,
-} from './router.master.general.config';
+} from './router.general.config';
 
 export default [entrance, root, dashboard, currentAccount, result, notFound];
